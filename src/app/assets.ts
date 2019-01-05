84cd734bdf888a84b65aa045e30140d0c53d5cd6
Moustafa Mohsen <moustafamohsen1@gmail.com>
2019-01-05 16:05:16 +0200

    dyncamic work card

---
+++ src/app/assets.ts
@@ -0,0 +1,20 @@
+export const AppHost = location.protocol.concat("//").concat(window.location.host);
+
+
+const imagesPath="/assets/img/Framworks and platforms/work-card/"
+const mocksPath=imagesPath+"mocks/";
+
+const aspcore =`${AppHost}${imagesPath}`+ "angular.png";
+const angular =`${AppHost}${imagesPath}`+ "asp.net core.png";
+const github =`${AppHost}${imagesPath}`+ "github.png";
+const mysql =`${AppHost}${imagesPath}`+ "mysql.png";
+const fridgenotes_desktop =`${AppHost}${mocksPath}`+"fridgenotes-desktop.png";
+
+export const  Availableimages={
+    angular :`${AppHost}${imagesPath}`+ "angular.png",
+    aspcore :`${AppHost}${imagesPath}`+ "asp.net core.png",
+    github :`${AppHost}${imagesPath}`+ "github.png",
+    mysql :`${AppHost}${imagesPath}`+ "mysql.png",
+    digitalocean :`${AppHost}${imagesPath}`+ "digitalocean.png",
+    fridgenotes_desktop :`${AppHost}${mocksPath}`+"fridgenotes-desktop.png",
+}
--- src/app/work-section/work-section.component.ts
+++ src/app/work-section/work-section.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Availableimages } from '../assets';
 
 @Component({
   selector: 'app-work-section',
   templateUrl: './work-section.component.html',
   styleUrls: ['./work-section.component.css']
 })
+
 export class WorkSectionComponent implements OnInit {
 
   constructor() { }
-
+  workcard:workCard;
   ngOnInit() {
-  }
+    this.workcard={
+      title:"Fridge Nottes",
+      description:`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas voluptatem eveniet officiis maiores sit molestias architecto quae necessitatibus quidem veritatis iure voluptatum at, tempora tenetur non. Porro harum soluta fugiatt.`,
+
+      backgroundDesktop:Availableimages.fridgenotes_desktop,
+      languages:"Html, CSS, C#, TypeScript",
+      buttonText:"Visit Website",
+      resources_Images:[
+        Availableimages.angular,
+        Availableimages.aspcore,
+        Availableimages.mysql
+      ],
+      deployment_Images:[
+        Availableimages.github,
+        Availableimages.digitalocean
+      ],
+    };
+
+  }//ngOnInit
 
 }
+
+interface workCard{
+  title:string;
+  description:string;
+  resources_Images:string[];
+  deployment_Images:string[];
+  backgroundDesktop:string;
+  languages:string;
+  buttonText:string;
+}
+
+
+

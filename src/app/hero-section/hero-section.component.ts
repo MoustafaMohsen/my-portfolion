970162f03a40f696bafdf9baf8039034d0b6d189
Moustafa Mohsen <moustafamohsen1@gmail.com>
2019-01-27 15:03:56 +0200

    logo dynamic sizing

--- src/app/hero-section/hero-section.component.ts
+++ src/app/hero-section/hero-section.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-hero-section',
@@ -9,12 +10,21 @@ export class HeroSectionComponent implements OnInit {
 
   constructor() { }
   ngOnInit() {
-
   }
   getheight(){
     let style={
       height:window.innerHeight+'px'
     }
     return style
+  }
+
+  logoheight(){
+    let mmlogo = $('#mmlogo')
+    let logoWidth = mmlogo.width();
+    let style={
+      height: logoWidth / 3.5  +'px'
+    }
+    return style;
+    //mmlogo.css(style)
   }
 }
--- src/app/hero-section/mmlogo/mmlogo.component.ts
+++ src/app/hero-section/mmlogo/mmlogo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from "jquery";
 @Component({
   selector: 'app-mmlogo',
   templateUrl: './mmlogo.component.html',
@@ -10,6 +10,15 @@ export class MMLogoComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  wordHeight(){
+    let wordBox = $('#word-box');
+    let height = wordBox.height();
+    let style={
+      'font-size': height + 'px',
+      'line-height': height + 'px'
+    }
+    return style;
   }
 
 }
